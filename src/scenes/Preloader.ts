a47f20208e8ee4df8a1b92758a23483ea6c60d80
Flynn Rundquist <flynn.rundquist@mths.ca>
2024-06-07 10:05:03 -0400

    fixed sprites (maybe?)

--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -47,4 +47,7 @@ export class Preloader extends Scene
         //  Move to the MainMenu. You could also swap this for a Scene Transition, such as a camera fade.
         this.scene.start('MainMenu');
     }
+
 }
+
+export default Preloader;
