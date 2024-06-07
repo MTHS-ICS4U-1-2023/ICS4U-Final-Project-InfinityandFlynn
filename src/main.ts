d4f6ba1049d59587f40dee2ff6f117016556eb8d
Flynn Rundquist <flynn.rundquist@mths.ca>
2024-06-07 08:57:45 -0400

    sprites and objects started

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { Boot } from './scenes/Boot';
 import { Game as MainGame } from './scenes/Game';
 import { GameOver } from './scenes/GameOver';
 import { MainMenu } from './scenes/MainMenu';
+import { Preloader } from './scenes/Preloader';
 
 import { Game, Types } from "phaser";
 
@@ -13,6 +14,13 @@ const config: Types.Core.GameConfig = {
     height: 768,
     parent: 'game-container',
     backgroundColor: '#028af8',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { x: 0, y: 300 },
+            debug: false
+        }
+    },
     scale: {
         mode: Phaser.Scale.FIT,
         autoCenter: Phaser.Scale.CENTER_BOTH
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -21,17 +21,7 @@ export class Game extends Scene
         this.background = this.add.image(512, 384, 'background');
         this.background.setAlpha(0.5);
 
-        this.msg_text = this.add.text(512, 384, 'Make something fun!\nand share it with us:\nsupport@phaser.io', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        });
-        this.msg_text.setOrigin(0.5);
+        
 
-        this.input.once('pointerdown', () => {
-
-            this.scene.start('GameOver');
-
-        });
     }
 }
